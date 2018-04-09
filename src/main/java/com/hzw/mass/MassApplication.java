fb211287a970baf019f97da9f9aa914ad5640a3c
1393467261@qq.com <1393467261@qq.com>
2018-04-09 10:22:46 +0800

    add comment

--- src/main/java/com/hzw/mass/MassApplication.java
+++ src/main/java/com/hzw/mass/MassApplication.java
@@ -3,7 +3,7 @@ package com.hzw.mass;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
-*@Description: 
+*@Description: 程序入口
 */
 @SpringBootApplication
 public class MassApplication {
